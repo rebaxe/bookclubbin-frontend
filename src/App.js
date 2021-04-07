903d6eb69cecbd7e51f675c2ed209c52a1c25772
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-04-07 14:52:07 +0200

    Add route to search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Search from './components/Search/Search.js'
 import NavBar from './components/NavBar/NavBar.js'
+import Home from './components/Home/Home.js'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Search />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return ( 
+    <div className="homePage">
+      <h1>HOME</h1>
+      <a href='/search'>Search</a>
+    </div>
+   );
+}
+ 
+export default Home;
