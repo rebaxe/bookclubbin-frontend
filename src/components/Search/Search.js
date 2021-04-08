2443c7ee53af7942d2a2e77ec653739efba5b427
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-04-08 11:04:26 +0200

    Search books connected to backend

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,9 @@
-import { FormControl, FormHelperText, InputAdornment, InputLabel, OutlinedInput } from '@material-ui/core'
+import { Button, FormControl, FormHelperText, InputLabel, OutlinedInput } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 import { makeStyles } from '@material-ui/core/styles'
 import googleImg from './images/google-attribution.png'
+import axios from 'axios'
+import { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
   searchField: {
@@ -21,25 +23,56 @@ const useStyles = makeStyles((theme) => ({
 
 const Search = () => {
   const classes = useStyles()
+  const [query, setQuery] = useState('')
+  
   const handleSearch = (e) => {
-    console.log('You clicked search!')
-    console.log(e.target.value)
+    e.preventDefault()
+    console.log(query)
+    const URL = process.env.REACT_APP_SEARCH_URL
+    // const apiUrl = `http://localhost:8080/api/v1/search`
+    axios.get(URL, { 
+      params: { query: query }
+    }, {
+      headers: {
+        Accept: 'application/json',
+       'Content-Type': 'application/json'
+      }
+    }, {
+      body: JSON.stringify(query)
+    }).then(res => {
+      const books = res.data
+      console.log(books)
+    }).catch((error) => {
+      console.log(error.message)
+    })
   }
+
+  //useEffect(() => {
+  //  console.log(query)
+  //  const apiUrl = `http://localhost:8080/api/v1/search`
+  //  axios.get(apiUrl, {
+  //    body: JSON.stringify(query)
+  //  }).then(res => {
+  //    const books = res.data
+  //    console.log(books)
+  //  }).catch((error) => {
+  //    console.log(error.message)
+  //  })
+  //})
+
   return (
-    <FormControl className={classes.searchField} variant="outlined">
+    <form  onSubmit={handleSearch}>
+      <FormControl className={classes.searchField}>
       <InputLabel htmlFor="search-books">Search books</InputLabel>
         <OutlinedInput
           id="search-books"
-          startAdornment={ 
-            <InputAdornment position="start">
-              <SearchIcon/>
-            </InputAdornment>
-          }
           labelWidth={100}
-          onChange={handleSearch}
+          onChange={(e) => setQuery(e.target.value)}
         />
       <FormHelperText className={classes.helperText}>*Search by title or author.<img className={classes.image} src={googleImg} alt="Powered by Google"></img></FormHelperText>
+    <Button type="submit"><SearchIcon/></Button>
     </FormControl>
+    </form>
   )
 }
 export default Search
