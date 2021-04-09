d79eff22ceb149696ec28bf83fc9a4c7be1ba371
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-04-09 10:03:05 +0200

    Add styling to modal

--- src/components/Search/BookModal.js
+++ src/components/Search/BookModal.js
@@ -1,6 +1,8 @@
 import { Modal, Fade, Button } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
+import { ClassSharp } from "@material-ui/icons"
 import CloseIcon from '@material-ui/icons/Close'
+import googleImg from './images/google-attribution.png'
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -14,7 +16,29 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 5,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    margin: theme.spacing(2)
+    margin: theme.spacing(2),
+    position: 'relative',
+    width: '80%'
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    padding: 5,
+  },
+  bookInfoContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    color: '#333',
+    fontSize: '0.8rem',
+    gap: theme.spacing(2)
+  },
+  googleImage: {
+    height: '10px',
+    padding: '10px',
+    position: 'absolute',
+    bottom: 10,
+    right: 10
   },
 }))
 
@@ -30,20 +54,23 @@ const BookModal = (props) => {
         aria-describedby="simple-modal-description"
         className={classes.modal}
       >
-      <Fade in={view}>
+      <Fade in={view} >
         <div className={classes.paper}>
-          <Button onClick={() => {handleClose()}}>
+          <Button className={classes.closeBtn} onClick={() => {handleClose()}}>
             <CloseIcon />
           </Button>
           {book.image && <img src={book.image} alt={`Cover for ${book.title}`}/>}
           <h2 id="transition-modal-title">{book.title}</h2>
           {book.authors && <h2 id="transition-modal-title"> by {book.authors} </h2>}
           <p id="transition-modal-description">{book.description}</p>
-          {book.pages && <h3>Pages: {book.pages}</h3>}
-          {book.googleRating && <h3>Google Rating: {book.googleRating}</h3>}
+          <div className={classes.bookInfoContainer}>
+            {book.pages && <span>Pages: {book.pages}</span>}
+            {book.googleRating && <span>Google Rating: {book.googleRating}</span>}
+            <img className={classes.googleImage} src={googleImg} alt="Google Attribution"/>
+          </div>
         </div>
       </Fade>
-      </Modal>
+    </Modal>
    );
 }
  
